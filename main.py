c0fb5c2955575a993886ebcbb29ad06d979e3142
Parth <parth.gupta023@nmims.edu.in>
2022-01-08 13:28:33 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config['SECRET_KEY'] = os.environ.get("BLOG_SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
